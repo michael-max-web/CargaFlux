0c730a2464960a27a00bac5cd11c5502caa4bd74
MICHAEL DOUGLAS SOUSA CRUZ <michael_d_cruz@estudante.sesisenai.org.br>
2025-10-27 19:01:26 -0300

    atualização

---
+++ cargaFlux.js
@@ -0,0 +1,10 @@
+let usuario; senha
+function loginUsuario(){
+    
+usuario = document.getElementById("inputUsuario").value 
+document.getElementById("inputUsuario").value = ''   
+
+senha = document.getElementById("inputSenha").value 
+document.getElementById("inputSenha").value = ''
+
+}
